65c5b65b3add536f4cde804bb445c597d193647c
rheiformes <rhearai77@gmail.com>
2023-02-13 11:20:30 -0600

    Initial Commit

---
+++ IP12_Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IP12_Trivia
+//
+//  Created by Rai, Rhea on 2/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
